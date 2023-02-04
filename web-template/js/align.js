5bbf40feedae6568dfd34936ee2ce48d9d3b984e
Evan Evanebenezer Ebenezer <Evanebenezer@OSX2309.local>
2023-02-04 10:45:25 -0500

    Django framework; search function

--- web-template/js/align.js
+++
@@ -1,5 +0,0 @@
-const nav = document.querySelector("nav");
-const main = document.querySelector("main");
-
-// content so that there's no gap between navbar
-main.style.marginTop = nav.clientHeight.toString() + "px";
